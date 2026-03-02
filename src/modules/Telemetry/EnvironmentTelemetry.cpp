d28acb51e8d1ebe55344bace5e50eb9874d826aa
ghegem <gh.ghem@aesopsfables.net>
2026-03-03 04:14:49 +0800

    Update EnvironmentTelemetry.cpp
    t-echo temp/humidity offset adjustment calibrated with mijia room sensor

--- src/modules/Telemetry/EnvironmentTelemetry.cpp
+++ src/modules/Telemetry/EnvironmentTelemetry.cpp
@@ -146,6 +146,17 @@ extern void drawCommonHeader(OLEDDisplay *display, int16_t x, int16_t y, const c
 #include "graphics/ScreenFonts.h"
 #include <Throttle.h>
 
+namespace
+{
+void applyOffset(meshtastic_EnvironmentMetrics *metrics)
+{
+    if (metrics->has_temperature)
+        metrics->temperature -= 1.6;  //temp reading offset (-1.8 previous value)
+    if (metrics->has_relative_humidity)
+        metrics->relative_humidity += 16.5;  //humidity reading offset (+18.5 previous value)
+}
+} // namespace
+
 static constexpr uint16_t TX_HISTORY_KEY_ENVIRONMENT_TELEMETRY = 0x8002;
 
 void EnvironmentTelemetryModule::i2cScanFinished(ScanI2C *i2cScanner)
@@ -580,6 +591,9 @@ bool EnvironmentTelemetryModule::getEnvironmentTelemetry(meshtastic_Telemetry *m
         hasSensor = true;
     }
 #endif
+    if (valid)
+        applyOffset(&m->variant.environment_metrics);
+  
     return valid && hasSensor;
 }
 
