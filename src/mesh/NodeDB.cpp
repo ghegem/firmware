f7f4dff502f973b0723466a529fbc0b187fe8e8f
jem0n <123236915+jem0n@users.noreply.github.com>
2026-03-24 15:07:22 +0800

    Update NodeDB.cpp

--- src/mesh/NodeDB.cpp
+++ src/mesh/NodeDB.cpp
@@ -1226,6 +1226,8 @@ void NodeDB::pickNewNodeNum()
             NodeNum customnodeNum = ((uint32_t)tempMac[2] << 24) | ((uint32_t)tempMac[3] << 16) | ((uint32_t)tempMac[4] << 8) | (uint32_t)tempMac[5];
             nodeNum = customnodeNum;
 
+            snprintf(owner.long_name, sizeof(owner.long_name), "Meshtastic %04x", getNodeNum() & 0x0ffff);
+            snprintf(owner.short_name, sizeof(owner.short_name), "%04x", getNodeNum() & 0x0ffff);
             // Update the global node number state
             //myNodeInfo.my_node_num = nodeNum;
         
